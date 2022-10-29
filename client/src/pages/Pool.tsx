3ccc8a59d3b6f748c94bb511650852c4e19644dd
dbdpfls <year9882@gmail.com>
2022-10-29 22:52:37 +0900

    add: pool화면 및 로고 이미지 추가

---
+++ client/src/pages/Pool.tsx
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import { TokenList } from "../types";
+import ThemeContext from "../context/theme-context";
+import {useMoralis} from "react-moralis";
+import { useTranslation } from "react-i18next";
+import Moralis from "moralis";
+import { ChainHex, TransactionList } from "../types";
+import ChainContext from "../context/chain-context";
+
+type PoolProps = {
+    setLoginModalOpen(val: boolean): void;
+};
+
+const Pool = ({
+                  setLoginModalOpen,
+              }: PoolProps): JSX.Element => {
+    const {isLight} = React.useContext(ThemeContext);
+    const { isAuthenticated } = useMoralis();
+
+    return (
+        <div className="flex items-center justify-center flex-grow">
+            {!isAuthenticated && (
+                <div className={isLight ? styles.light : styles.dark}>
+                    <button
+                        className={isLight ? styles.connectLight : styles.connectDark}
+                        onClick={() => setLoginModalOpen(true)}
+                    >
+                        {("Create Pool")}
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Pool;
+
+const styles = {
+    light:
+        "border-2 border-orange-400 bg-orange-400 rounded-3xl h-[300px] w-11/12 sm:w-[300px] flex justify-center items-center",
+    dark: "border-2 border-blue-700 bg-blue-700 rounded-3xl h-[300px] w-11/12 sm:w-[300px] flex justify-center items-center",
+    connectLight:
+        "bg-orange-300 w-[150px] h-[60px] rounded-2xl text-white font-bold border-white border-2",
+    connectDark:
+        "bg-blue-500 w-[150px] h-[60px] rounded-2xl text-white font-bold border-white border-2",
+    txLight:
+        "border-2 border-orange-400 bg-orange-400 rounded-3xl h-[300px] w-[300px] sm:w-[500px] sm:h-[500px]",
+    txDark:
+        "border-2 border-blue-500 bg-blue-500 rounded-3xl h-[300px] w-[300px] sm:w-[500px] sm:h-[500px]",
+};
